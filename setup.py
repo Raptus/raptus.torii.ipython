84d6aaa470c5e976a0278805c08b4e33ed236b3c
Jonas Wagner <jwagner@raptus.com>
2012-11-05 13:20:46 +0100

    Moved to github

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ version = '1.0b1'
 setup(name='raptus.torii.ipython',
       version=version,
       description="extends raptus.torii with ipython",
-      long_description=open("README.txt").read() + "\n" +
+      long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
